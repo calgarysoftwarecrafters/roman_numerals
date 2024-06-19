e23c1ecc82fb352c238535dd37c7c65ed6a77343
Prashant Sharma <132094210+prs-sharma9@users.noreply.github.com>
2024-06-18 22:59:05 -0600

    Java Test Project

---
+++ Java/src/main/Driver.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Driver {
+
+	public String intToRoman(int num) {
+		
+		// Implement logic to convert x into Roman Number.
+
+	}
+
+}
---
+++ Java/test/main/DriverTest.java
@@ -0,0 +1,74 @@
+package main;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class DriverTest {
+
+	
+	@ParameterizedTest
+	@CsvSource({
+		"1, I",
+	    "2, II",
+	    "3, III",
+	    "4, IV",
+	    "5, V",
+	    "6, VI",
+	    "7, VII",
+	    "8, VIII",
+	    "9, IX",
+	    "10, X",
+	    "11, XI",
+	    "12, XII",
+	    "13, XIII",
+	    "14, XIV",
+	    "15, XV",
+	    "16, XVI",
+	    "17, XVII",
+	    "18, XVIII",
+	    "19, XIX",
+	    "20, XX",
+	    "21, XXI",
+	    "22, XXII",
+	    "23, XXIII",
+	    "24, XXIV",
+	    "25, XXV",
+	    "26, XXVI",
+	    "27, XXVII",
+	    "28, XXVIII",
+	    "29, XXIX",
+	    "30, XXX",
+	    "31, XXXI",
+	    "32, XXXII",
+	    "33, XXXIII",
+	    "34, XXXIV",
+	    "35, XXXV",
+	    "36, XXXVI",
+	    "37, XXXVII",
+	    "38, XXXVIII",
+	    "39, XXXIX",
+	    "40, XL",
+	    "41, XLI",
+	    "42, XLII",
+	    "100, C",
+	    "200, CC",
+	    "300, CCC",
+	    "400, CD",
+	    "500, D",
+	    "600, DC",
+	    "700, DCC",
+	    "800, DCCC",
+	    "900, CM",
+	    "1000, M",
+	    "2000, MM",
+	    "3000, MMM",
+	    "3999, MMMCMXCIX"
+	})
+	void test_intToRoman(int input, String expected) {
+		Driver driverObject = new Driver();
+		String actual = driverObject.intToRoman(input);
+		
+		Assertions.assertTrue(expected.equals(actual));
+	}
+}
